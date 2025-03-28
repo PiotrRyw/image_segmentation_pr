f1f8ebc2d9a243116ff737988a6cada67290027b
Piotr <p.rywczak@gmail.com>
2025-03-28 14:00:38 +0100

    Added multiprocessing and displaying loss graph. Changed the way ModelOps is used and now it relies on instance methods instead of class methods

--- scripts/img_seg_Ops.py
+++ scripts/img_seg_Ops.py
@@ -1,33 +1,27 @@
 import json
-import random
+import os
 import datetime
+from multiprocessing import Queue
 from pathlib import Path
 from typing import List, Tuple
-
 import pandas as pd
+from functools import partial
+from distinctipy import distinctipy
 
 import torch
 import torchvision
-from torch import nn
-from torchvision import datasets
-from torchvision.models.detection import maskrcnn_resnet50_fpn_v2
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
-from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 from cjm_pandas_utils.core import markdown_to_pandas
 from torch.utils.data import DataLoader, Dataset
 from torchvision.utils import draw_bounding_boxes
 from torchtnt.utils import get_module_summary
-
 from cjm_psl_utils.core import download_file
 
-from functools import partial
-from distinctipy import distinctipy
 
 from scripts.img_seg_dataset import COCOLIVECellDataset, DatasetUtils
 from scripts.project_utils import train_tfms, verify_dataset, tuple_batch, draw_learning_graph
 from scripts.segmenting_images import segment_image
-from scripts.test_model import test_model
-from scripts.train import train_loop
+from scripts.testing_model import test_model
+from scripts.training_model import train_loop
 from scripts.network_design import NeuralNetworkOpsBaseClass, NeuralNetworkOps
 
 
@@ -68,6 +62,7 @@ class State:
             "image_dict": {},
             "prediction_threshold": 0.0,
             "checkpoint_dir": Path(),
+            "checkpoint_path" : Path(),
             "prediction_model_path": "",
             "image_path": ""
         }
@@ -112,8 +107,11 @@ class State:
 
 class ModelOpsUtils:
     @staticmethod
-    def make_config_dir_save_color_map(color_map, checkpoint_dir, model_name, dataset_path_name):
+    def make_config_dir_save_color_map(color_map, checkpoint_dir: Path, model_name, dataset_path_name):
         # Create the checkpoint directory if it does not already exist
+        if not isinstance(checkpoint_dir, Path):
+            raise RuntimeError
+
         checkpoint_dir.mkdir(parents=True, exist_ok=True)
 
         with open(f"{checkpoint_dir}/{dataset_path_name}-colormap.json", "w") as file:
@@ -122,84 +120,84 @@ class ModelOpsUtils:
         # Print the name of the file that the color map was written to
         print(f"{checkpoint_dir}/{dataset_path_name}-colormap.json")
 
+
 class ModelOps:
-    state = State()
+    _instances = 0
+    def __init__(self):
+        if ModelOps._instances > 0:
+            del self
+            raise RuntimeError("Tried to create more than one instance of ModelOps")
+        ModelOps._instances += 1
 
-    @staticmethod
-    def check_readiness(func):
-        def wrapper(*args,**kwargs):
-            is_ready = True
-            missing = []
-            if func.__name__ == "load_data":
-                data_can_be_loaded, temp = ModelOps.state.data_can_be_loaded
-                is_ready = is_ready and data_can_be_loaded
-                missing.append(temp)
+        self.state = State()
+        self.queue = None
 
-            if func.__name__ in ["train", "test"]:
-                data_is_loaded, temp = ModelOps.state.data_is_loaded
-                missing.append(temp)
-                is_ready = is_ready and data_is_loaded
+    # @staticmethod
+    # def check_readiness(func):
+    #     def wrapper(*args,**kwargs):
+    #         is_ready = True
+    #         missing = []
+    #         if func.__name__ == "load_data":
+    #             data_can_be_loaded, temp = ModelOps.state.data_can_be_loaded
+    #             is_ready = is_ready and data_can_be_loaded
+    #             missing.append(temp)
+    #
+    #         if func.__name__ in ["train", "test"]:
+    #             data_is_loaded, temp = ModelOps.state.data_is_loaded
+    #             missing.append(temp)
+    #             is_ready = is_ready and data_is_loaded
+    #
+    #         if not is_ready:
+    #             print(f"Missing following: {missing} from config file")
+    #             return
+    #
+    #         func(*args, **kwargs)
+    #
+    #     return wrapper
 
-            if not is_ready:
-                print(f"Missing following: {missing} from config file")
-                return
 
-            func(*args, **kwargs)
+    def load_sample_image(self):
+        self.load_data()
+        verify_dataset(self.state["training_data"], self.state["class_names"], self.state["font_file"], 1)
+        verify_dataset(self.state["test_data"], self.state["class_names"], self.state["font_file"], 1)
 
-        return wrapper
 
-    @classmethod
-    @check_readiness
-    def load_sample_image(cls):
-        ModelOps.load_data()
-        verify_dataset(cls.state["training_data"], cls.state["class_names"], cls.state["font_file"], 1)
-        verify_dataset(cls.state["test_data"], cls.state["class_names"], cls.state["font_file"], 1)
-
-    @classmethod
-    def setup_model(cls, model_path=""):
+    def setup_model(self, model_path=""):
         # select device
         device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
-        cls.state["device"] = device
+        self.state["device"] = device
         print(f"Using {device} device")
 
         dtype = torch.float32
         # Model
-        cls.state["model"] = NeuralNetworkOps(
+        self.state["model"] = NeuralNetworkOps(
             device=device,
             dtype=dtype,
-            number_of_classes=len(cls.state["class_names"]),
+            number_of_classes=len(self.state["class_names"]),
             model_path=model_path
         )
 
-    @classmethod
-    # @check_readiness
-    def train(cls):
+
+    def train(self, checkpoint_dir, checkpoint_path: Path):
         # Training
 
-        # Create timestamp and directory to store training data and best model
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        color_map = {'items': [{'label': label, 'color': color} for label, color in
+                               zip(self.state["class_names"], self.state["colors"])]}
 
-        # Create a color map
-        color_map = {'items': [{'label': label, 'color': color} for label, color in zip(cls.state["class_names"], cls.state["colors"])]}
-
-        checkpoint_dir: Path = cls.state["project_dir"] / timestamp
-        cls.state["checkpoint_dir"] = checkpoint_dir
-        # The model checkpoint path
-        checkpoint_path: Path = checkpoint_dir / f"{cls.state['model'].name}.pth"
 
         ModelOpsUtils.make_config_dir_save_color_map(color_map=color_map,
                                                      checkpoint_dir=checkpoint_dir,
-                                                     model_name=cls.state['model'].name,
-                                                     dataset_path_name=cls.state["dataset_path"].name,
+                                                     model_name=self.state['model'].model.name,
+                                                     dataset_path_name=self.state["dataset_path"].name,
                                                      )
 
         # Learning rate for the model
-        lr = cls.state["initial_learning_rate"]
+        lr = self.state["initial_learning_rate"]
 
         # Number of training epochs
-        epochs = cls.state["number_of_epochs"]
+        epochs = self.state["number_of_epochs"]
 
-        model = cls.state["model"].model
+        model = self.state["model"].model
 
         # AdamW optimizer; includes weight decay for regularization
         optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
@@ -207,7 +205,7 @@ class ModelOps:
         # Learning rate scheduler; adjusts the learning rate during training
         lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer,
                                                            max_lr=lr,
-                                                           total_steps=epochs * len(cls.state["dataloader_training_data"]))
+                                                           total_steps=epochs * len(self.state["dataloader_training_data"]))
 
         columns = ['epoch', 'train_loss', 'valid_loss', 'learning_rate', 'model_architecture']
         epoch_df = pd.DataFrame({}, columns=columns)
@@ -215,67 +213,65 @@ class ModelOps:
         epoch_df.to_csv(temp_path, mode='a', index=False, header=True)
 
         train_loop(model=model,
-                   train_dataloader=cls.state["dataloader_training_data"],
-                   valid_dataloader=cls.state["dataloader_testing_data"],
+                   train_dataloader=self.state["dataloader_training_data"],
+                   valid_dataloader=self.state["dataloader_testing_data"],
                    optimizer=optimizer,
                    lr_scheduler=lr_scheduler,
-                   device=torch.device(cls.state["device"]),
+                   device=torch.device(self.state["device"]),
                    epochs=epochs,
                    checkpoint_path=checkpoint_path,
-                   train_sz=cls.state["train_data_size"],
-                   batch_size=cls.state["batch_size"],
-                   initial_learning_rate=cls.state["initial_learning_rate"],
-                   annotation_file_path=cls.state["annotation_file_path"],
-                   use_scaler=True)
+                   train_sz=self.state["train_data_size"],
+                   batch_size=self.state["batch_size"],
+                   initial_learning_rate=self.state["initial_learning_rate"],
+                   annotation_file_path=self.state["annotation_file_path"],
+                   queue=self.queue,
+                   use_scaler=True,
+                   )
 
 
-    @classmethod
-    @check_readiness
-    def test(cls, checkpoint_dir):
-        model = cls.state["model"].model
+    def test(self, checkpoint_dir):
+        model = self.state["model"].model
         model.eval()
 
         print(checkpoint_dir)
 
         log_path = checkpoint_dir / "log.csv"
 
-        draw_learning_graph(log_path)
+        # draw_learning_graph(log_path)
 
         # Make a copy of the color map in integer format
-        int_colors = [tuple(int(c * 255) for c in color) for color in cls.state["colors"]]
+        int_colors = [tuple(int(c * 255) for c in color) for color in self.state["colors"]]
 
         test_model(model=model,
-                   device=torch.device(cls.state["device"]),
-                   val_keys = cls.state["validation_keys"],
-                   image_dict = cls.state["image_dict"],
-                   train_data_size = cls.state["train_data_size"],
-                   annotation_df = cls.state["annotation_df"],
-                   threshold = cls.state["prediction_threshold"],
-                   class_names = cls.state["class_names"],
+                   device=torch.device(self.state["device"]),
+                   val_keys = self.state["validation_keys"],
+                   image_dict = self.state["image_dict"],
+                   train_data_size = self.state["train_data_size"],
+                   annotation_df = self.state["annotation_df"],
+                   threshold = self.state["prediction_threshold"],
+                   class_names = self.state["class_names"],
                    int_colors = int_colors,
-                   font=cls.state["font_file"],
+                   font=self.state["font_file"],
                    )
 
 
-    @classmethod
-    @check_readiness
-    def load_data(cls):
+    def load_data(self):
         # Download the font file
-        download_file(f"https://fonts.gstatic.com/s/roboto/v30/{cls.state['font_file']}", "./")
-        draw_bboxes = partial(draw_bounding_boxes, fill=False, width=2, font=cls.state["font_file"], font_size=10)
+        download_file(f"https://fonts.gstatic.com/s/roboto/v30/{self.state['font_file']}", "./")
+        draw_bboxes = partial(draw_bounding_boxes, fill=False, width=2, font=self.state["font_file"], font_size=10)
 
-        dataset_name = cls.state["dataset_name"]
-        dataset_dir = cls.state["dataset_dir"]
-        images_subdirectory = cls.state["images_subdirectory"]
-        annotation_file_path = cls.state["annotation_file_path"]
-        class_names = cls.state["class_names"]
-        train_pct = cls.state["train_pct"]
+        dataset_name = self.state["dataset_name"]
+        dataset_dir = self.state["dataset_dir"]
+        images_subdirectory = self.state["images_subdirectory"]
+        annotation_file_path = self.state["annotation_file_path"]
+        class_names = self.state["class_names"]
+        train_pct = self.state["train_pct"]
 
-        cls.state["dataset_path"] = Path(f'{dataset_dir}/{dataset_name}')
-        dataset_path = cls.state["dataset_path"]
+        self.state["dataset_path"] = Path(f'{dataset_dir}/{dataset_name}')
+        dataset_path = self.state["dataset_path"]
         image_directory = dataset_path / images_subdirectory
 
-        cls.state["project_dir"] = Path(f"./{cls.state['project_name']}/")
+        self.state["project_dir"] = Path(f"./{self.state['project_name']}/")
 
         # Creating a Series with the paths and converting it to a DataFrame for display
         dataframe = pd.Series({
@@ -290,31 +286,39 @@ class ModelOps:
             train_pct,
         )
 
-        cls.state["image_dict"] = image_dict
-        cls.state["annotation_df"] = annotation_df
-        cls.state["colors"] = distinctipy.get_colors(len(class_names))
+        self.state["image_dict"] = image_dict
+        self.state["annotation_df"] = annotation_df
+        self.state["colors"] = distinctipy.get_colors(len(class_names))
 
-        cls.state["validation_keys"] = val_keys
+        self.state["validation_keys"] = val_keys
 
-        cls.state["training_data"] = COCOLIVECellDataset(train_keys, annotation_df, image_dict, class_to_idx,
-                                        train_tfms)
-        cls.state["test_data"] = COCOLIVECellDataset(val_keys, annotation_df, image_dict, class_to_idx,
-                                    train_tfms)
+        self.state["training_data"] = COCOLIVECellDataset(train_keys, annotation_df, image_dict, class_to_idx,
+                                                          train_tfms)
+        self.state["test_data"] = COCOLIVECellDataset(val_keys, annotation_df, image_dict, class_to_idx,
+                                                      train_tfms)
 
         temp = pd.Series({
-            'Training dataset size:': len(cls.state["training_data"]),
-            'Validation dataset size:': len(cls.state["test_data"])}
+            'Training dataset size:': len(self.state["training_data"]),
+            'Validation dataset size:': len(self.state["test_data"])}
         ).to_frame()
         print(temp.to_string())
 
+        # Create timestamp and directory to store training data and best model
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-    @classmethod
-    def run_epochs(cls):
+        checkpoint_dir: Path = self.state["project_dir"] / timestamp
+        self.state["checkpoint_dir"] = checkpoint_dir
 
+        print("Saving dir")
+        print(str(checkpoint_dir))
+        checkpoint_dir.mkdir(parents=True, exist_ok=True)
+
+
+    def get_ready(self):
         # select device
-        cls.setup_model()
-        device = cls.state["device"]
-        model = cls.state["model"].model
+        self.setup_model()
+        device = self.state["device"]
+        model = self.state["model"].model
 
         test_inp = tuple(torch.randn(1, 3, 256, 256).to(device))
         print(test_inp)
@@ -330,11 +334,11 @@ class ModelOps:
 
         # DataLoader
         # Set the training batch size
-        bs = cls.state["batch_size"]
+        bs = self.state["batch_size"]
 
         # Set the number of worker processes for loading data.
         # num_workers = multiprocessing.cpu_count() // 2
-        num_workers = cls.state["number_of_workers"]
+        num_workers = self.state["number_of_workers"]
 
         # Define parameters for DataLoader
         data_loader_params = {
@@ -350,37 +354,40 @@ class ModelOps:
         }
 
         # Create DataLoader for training data. Data is shuffled for every epoch.
-        cls.state["dataloader_training_data"] = DataLoader(cls.state["training_data"], **data_loader_params, shuffle=True)
+        self.state["dataloader_training_data"] = DataLoader(self.state["training_data"], **data_loader_params, shuffle=True)
 
         # Create DataLoader for validation data. Shuffling is not necessary for validation data.
-        cls.state["dataloader_testing_data"] = DataLoader(cls.state["test_data"], **data_loader_params)
+        self.state["dataloader_testing_data"] = DataLoader(self.state["test_data"], **data_loader_params)
 
         # Print the number of batches in the training and validation DataLoaders
         temp = pd.Series({
-            'Number of batches in train DataLoader:': len(cls.state["dataloader_training_data"]),
-            'Number of batches in validation DataLoader:': len(cls.state["dataloader_testing_data"])}
+            'Number of batches in train DataLoader:': len(self.state["dataloader_training_data"]),
+            'Number of batches in validation DataLoader:': len(self.state["dataloader_testing_data"])}
         ).to_frame().style.hide(axis='columns')
 
-        cls.train()
+        self.state["checkpoint_path"] = self.state["checkpoint_dir"] / f"{self.state['model'].model.name}.pth"
 
-        cls.test(cls.state["checkpoint_dir"])
+    def run_epochs(self):
+        self.train(self.state["checkpoint_dir"], self.state["checkpoint_path"])
+        self.test(self.state["checkpoint_dir"])
 
-    @classmethod
-    def test_on_random_sample(cls):
-        cls.load_data()
-        cls.setup_model()
-        cls.test(cls.state["checkpoint_dir"])
+    def get_queue(self, q: Queue):
+        self.queue = q
 
-    @classmethod
-    def infer(cls):
+    def test_on_random_sample(self):
+        self.load_data()
+        self.setup_model()
+        self.test(self.state["checkpoint_dir"])
 
-        cls.setup_model(cls.state["prediction_model_path"])
+
+    def infer(self):
+
+        self.setup_model(self.state["prediction_model_path"])
 
         model_settings = {
-            "class_names": cls.state["class_names"],
-            "train_size": cls.state["train_data_size"],
-            "device": cls.state["device"],
-            "threshold": cls.state["prediction_threshold"]
+            "class_names": self.state["class_names"],
+            "train_size": self.state["train_data_size"],
+            "device": self.state["device"],
+            "threshold": self.state["prediction_threshold"]
         }
-        segment_image(model=cls.state["model"], image_path=cls.state["image_path"], model_settings=model_settings)
-
+        segment_image(model=self.state["model"].model, image_path=self.state["image_path"], model_settings=model_settings)
--- scripts/img_seg_app.py
+++ scripts/img_seg_app.py
@@ -1,57 +1,164 @@
 import json
-import torch
+import math
+import os.path
+import time
+from multiprocessing import Process, Queue
+from pathlib import Path
+from queue import Empty
 
-from scripts.img_seg_model import ModelOps
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
 
+from scripts.img_seg_Ops import ModelOps
+from scripts.project_utils import prepare_learning_graph
+
+_DEBUG = True
 
 class ImageSegApplication:
     """Class loading settings from config file and starting either training or inference"""
 
-    config_path = "scripts/config.json"
-    def __init__(self):
-        self._load_json_config(ImageSegApplication.config_path)
+    default_config_path = "scripts/config.json"
+
+    def __init__(self, config: str = None):
+        self.model_ops = None
+        if not config:
+            self.config_path = ImageSegApplication.default_config_path
+        else:
+            self.config_path = config
 
     def _load_json_config(self, path):
         """Loading config file with settings from path specified in cls field [config_path]"""
         with open(path, "r") as file:
             temp = json.load(file)
-        print(temp)
-        ModelOps.state["batch_size"] = temp["Training_settings"]["batch_size"]
-        ModelOps.state["number_of_epochs"] = temp["Training_settings"]["number_of_epochs"]
-        ModelOps.state["model_path"] = temp["Training_settings"]["model_path"]
-        ModelOps.state["number_of_workers"] = temp["Training_settings"]["number_of_workers"]
-        ModelOps.state["train_data_size"] =  temp["Training_settings"]["train_data_size"]
 
-        ModelOps.state["project_name"] = temp["Training_settings"]["project_name"]
-        ModelOps.state["train_pct"] = temp["Training_settings"]["training_split_ratio"]
-        ModelOps.state["initial_learning_rate"] = temp["Training_settings"]["initial_learning_rate"]
+        self.model_ops.state["batch_size"] = temp["Training_settings"]["batch_size"]
+        self.model_ops.state["number_of_epochs"] = temp["Training_settings"]["number_of_epochs"]
+        self.model_ops.state["model_path"] = temp["Training_settings"]["model_path"]
+        self.model_ops.state["number_of_workers"] = temp["Training_settings"]["number_of_workers"]
+        self.model_ops.state["train_data_size"] =  temp["Training_settings"]["train_data_size"]
 
-        ModelOps.state["dataset_name"] = temp["Dataset"]["dataset_name"]
-        ModelOps.state["dataset_dir"] = temp["Dataset"]["dataset_dir"]
-        ModelOps.state["images_subdirectory"] = temp["Dataset"]["images_subdirectory"]
-        ModelOps.state["annotation_file_path"] = temp["Dataset"]["annotation_file_path"]
+        self.model_ops.state["project_name"] = temp["Training_settings"]["project_name"]
+        self.model_ops.state["train_pct"] = temp["Training_settings"]["training_split_ratio"]
+        self.model_ops.state["initial_learning_rate"] = temp["Training_settings"]["initial_learning_rate"]
 
-        ModelOps.state["prediction_threshold"] = temp["Prediction_settings"]["threshold"]
+        self.model_ops.state["dataset_name"] = temp["Dataset"]["dataset_name"]
+        self.model_ops.state["dataset_dir"] = temp["Dataset"]["dataset_dir"]
+        self.model_ops.state["images_subdirectory"] = temp["Dataset"]["images_subdirectory"]
+        self.model_ops.state["annotation_file_path"] = temp["Dataset"]["annotation_file_path"]
 
-        ModelOps.state["font_file"] = temp["Miscellaneous"]["font_file"]
+        self.model_ops.state["prediction_threshold"] = temp["Prediction_settings"]["threshold"]
 
-        ModelOps.state["prediction_model_path"] = temp["Prediction_settings"]["prediction_model_path"]
-        ModelOps.state["image_path"] = temp["Prediction_settings"]["image_path"]
-        ModelOps.state["class_names"] = temp["Training_settings"]["class_names"]
+        self.model_ops.state["font_file"] = temp["Miscellaneous"]["font_file"]
+
+        self.model_ops.state["prediction_model_path"] = temp["Prediction_settings"]["prediction_model_path"]
+        self.model_ops.state["image_path"] = temp["Prediction_settings"]["image_path"]
+        self.model_ops.state["class_names"] = temp["Training_settings"]["class_names"]
 
     def show_sample_data(self):
-        ModelOps.load_sample_image()
+        self.model_ops.load_sample_image()
+
+    def run_epochs(self, queue: Queue):
+        self.model_ops = ModelOps()
+        self._load_json_config(self.config_path)
+
+        self.model_ops.load_data()
+        self.model_ops.queue = queue
+        self.model_ops.get_ready()
+        self.model_ops.run_epochs()
 
     def run_training(self):
 
-        # load data
-        ModelOps.load_data()
-
+        queue = Queue()
         # run training and testing
-        ModelOps.run_epochs()
+        p_model_training = Process(target=self.run_epochs, args=(queue,))
+        p_display_progress = Process(target=self.display_progress, args=(queue,))
+
+        p_model_training.start()
+        p_display_progress.start()
+
+        p_model_training.join()
+
+        queue.put("Kill")
+
+        p_display_progress.join()
+
 
     def run_inference(self):
-        ModelOps.infer()
+        self.model_ops = ModelOps()
+        self._load_json_config(self.config_path)
+        self.model_ops.infer()
+
+    def _dummy_metadata_process(self, queue: Queue):
+
+        if os.path.exists("log.csv"):
+            os.remove("log.csv")
+
+        columns = ['epoch', 'train_loss', 'valid_loss', 'learning_rate', 'model_architecture']
+        epoch_df = pd.DataFrame({}, columns=columns)
+        temp_path = Path('log.csv')
+        epoch_df.to_csv(temp_path, mode='a', index=False, header=True)
+
+        for i in range(5):
+            epoch_metadata = {
+                'epoch': [i],
+                'train_loss': [math.cos(i)],
+                'valid_loss': [math.cos(i)],
+            }
+            epoch_df = pd.DataFrame(epoch_metadata)
+            temp_path = Path('log.csv')
+            epoch_df.to_csv(temp_path, mode='a', index=False, header=False)
+
+            queue.put(temp_path)
+            print("sent data")
+            time.sleep(1)
 
     def test_sample(self):
-        ModelOps.test_on_random_sample()
+        self.model_ops.test_on_random_sample()
+
+    def display_progress(self, queue: Queue):
+        plt.ion()  # Turn on interactive mode
+        fig, ax = plt.subplots()
+        while True:
+            try:
+                if len(plt.get_fignums()) == 0:
+                    fig, ax = plt.subplots()
+
+                path = queue.get(timeout=0.1)
+
+                if path == "Kill":
+                    break
+                if DEBUG:
+                    print(f"received: {path}")
+
+                all_data = pd.read_csv(path, index_col=0)
+                training_data = all_data[["train_loss", "valid_loss"]]
+
+                ax.clear()  # Clear previous data
+                ax.plot(training_data)
+                plt.draw()  # Draw the updated plot
+                plt.pause(0.1)  # Allow GUI event loop to update
+            except Empty:
+                pass
+            plt.gcf().canvas.flush_events()
+
+        plt.ioff()  # Turn off interactive mode
+        plt.show()  # Keep the final plot displayed
+
+
+if __name__ == "__main__":
+    app = ImageSegApplication("config.json")
+    queue = Queue()
+    # run training and testing
+
+    p_model_training = Process(target=app._dummy_metadata_process, args=(queue,))
+    p_display_progress = Process(target=app.display_progress, args=(queue,))
+
+    p_model_training.start()
+    p_display_progress.start()
+
+    p_model_training.join()
+
+    queue.put("Kill")
+
+    p_display_progress.join()
