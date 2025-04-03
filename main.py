03b94aae5283bcdb941ae678f3fa613dac572644
Piotr <p.rywczak@gmail.com>
2025-04-03 08:45:41 +0200

    added more run options to main

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from scripts.img_seg_app import ImageSegApplication
-
+from scripts.img_seg_dataset import test
 
 if __name__ == '__main__':
     app = ImageSegApplication()
-
     # app.show_sample_data()
-    app.run_training()
-
+    # app.run_training_main_process()
+    # app.test_sample()
+    app.run_inference()
+    # test()
